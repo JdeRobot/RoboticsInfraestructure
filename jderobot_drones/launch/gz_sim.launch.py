7f854f22d1eb16a9c1043bf953a0c923f9496e59
pariaspe <pedro.ariasp@upm.es>
2024-12-11 14:35:15 +0100

    add specific launcher for rescue people exercise

--- jderobot_drones/launch/gz_sim.launch.py
+++ jderobot_drones/launch/gz_sim.launch.py
@@ -17,31 +17,25 @@ from launch_ros.actions import Node, SetParameter
 def generate_launch_description():
     """Entrypoint"""
     launch_description = LaunchDescription([
-        DeclareLaunchArgument(
-            'world',
-            description='Gazebo world file'
-        ),
-        DeclareLaunchArgument(
-            'namespace',
-            description='Namespace to use'
-        ),
-        DeclareLaunchArgument(
-            'config_file',
-            description='Configuration file for the bridge'
-        ),
+        DeclareLaunchArgument('namespace',
+                              description='Namespace to use'),
+        DeclareLaunchArgument('world_file',
+                              description='Gazebo SDF world file'),
+        DeclareLaunchArgument('bridges_file',
+                              description='ROS-GZ bridge YAML file'),
         SetParameter(name='use_sim_time', value='true'),
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource([os.path.join(
                 get_package_share_directory('ros_gz_sim'), 'launch'),
                 '/gz_sim.launch.py']),
-            launch_arguments={'gz_args': ['-v 4 -s -r ', LaunchConfiguration('world')]}.items()),
+            launch_arguments={'gz_args': ['-v 4 -s -r ', LaunchConfiguration('world_file')]}.items()),
         Node(
             package='ros_gz_bridge',
             executable='parameter_bridge',
             namespace=LaunchConfiguration('namespace'),
             output='screen',
             parameters=[
-                {'config_file': LaunchConfiguration('config_file')},
+                {'config_file': LaunchConfiguration('bridges_file')},
             ]
         ),
         Node(
